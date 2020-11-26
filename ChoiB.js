fb98f0692b43cbee4b3ca7de5aece5a60e4def7d
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2020-11-26 16:50:17 -0500

    Update ChoiB.js

--- ChoiB.js
+++ ChoiB.js
@@ -1,6 +1,7 @@
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) { 
 
 if (msg == "ChoiB") {
+/*
 var fill=["알수없음", "충전중", "충전중 아님", "충전 완료 후 충전중이 아님", "충전 완료"];  
 var ifilter = new android.content.IntentFilter( android.content.Intent.ACTION_BATTERY_CHANGED);
 var batteryStatus = Api.getContext().registerReceiver(null, ifilter);
@@ -17,6 +18,19 @@ var ms2 = java.lang.System.currentTimeMillis();
 var ps = (((ms2-ms1)/1000) + "초");
 replier.reply ("[ChoiB]\n전원: 켜짐\n충전 상태: " + fill[battery-1] + "\n램: " + (mem.availMem/mem.totalMem*100).toFixed(2) + "% 남음\n배터리: " + Math.round(level/scale*100) + "%\n온도: " + Math.round(temp)/10 + "°C\n전압: " + voltage + "mv" + "\u200b".repeat(500) + "\n\nProject ChoiBKaKaoT By R. 체른\nCopyright (C) 2020 ChoiB By R. 체른\nAll rights reserved"); 
 }
-
+*/
+Device.getModelName(); //기기 모델명 반환
+Device.getPhoneModel(); //Device.getModelName();와 동일.
+Device.getPhoneBrand(); //기기 제조사 반환
+Device.getAndroidVersion(); //안드로이드 버전 반환
+Device.getBuild(); //android.os.Build(); 값 반환
+Device.getBatteryIntent(); //배터리 관련 Intent 반환
+Device.getBatteryLevel(); //배터리 % 잔량 반환
+Device.getBatteryTemp(); //배터리 섭씨 온도 반환
+Device.getBatteryHealth(); //배터리 건강 상태 반환
+Device.getBatteryStatus(); //배터리 상태 반환
+Device.getBatteryVoltage(); //배터리 전압 반환
+Device.isCharging(); //배터리가 충전중인지 반환.  
+  
 }
 }
