1fe358f22b019b2f793e7d29f926cc96c188536f
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2020-12-03 21:00:48 -0500

    1.3

--- 주사위.js
+++ 주사위.js
@@ -1,9 +1,9 @@
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) { 
-if (room == "뱁새봇") { 
+if (room == "지구b") { 
 
-if (msg == "뱁새주사위") {
+if (msg == "!주사위") {
 let dice = Math.floor(Math.random() * 6) + 1;
-replier.reply("[뱁새i] 주사위 결과 : " + dice); 
+replier.reply(sender + "(이)가 던진 주사위에서 [" + dice + "](이)가 나왔어~"); 
 }
 
 }
