cabab93a76db10b24ec230639f4e03bd46c3b2c7
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2020-12-03 22:00:01 -0500

    1.3

--- 쌉소리.js
+++ 쌉소리.js
@@ -3,10 +3,6 @@ const CH = {};
 const DB = {}; 
 const preMsg = {}; 
 
-var G1 = [];
-var G2 = [];
-var GHBlist = [];
-
 CH.say = function(msg, replier) { 
 replier.reply(msg); 
 };
@@ -22,21 +18,17 @@ return data[r];
 CH.isValidSender = function(sender){
 var noValid = ["방장봇", "봇", ".", "ㅎㅇ"]; 
 for (var n = 0; n < noValid.length; n++) { 
-if (data.startsWith(noValid[n])) return false;
-}
-noValid = ["방장봇", "봇", "."];
-for (var n = 0; n < noValid.length; n++) { 
-if (data.indexOf(noValid[n]) != -1) return false; 
+if (sender.indexOf(noValid[n]) != -1) return false; 
 }
 return true;
 };
 
 CH.isValidData = function(data) {
-var noSave = ["사진을 보냈습니다.", "동영상을 보냇습니다.", "음성메시지를 보냈습니다.", "카카오톡 프로필", "www.", "샵검색:", "#", "@", "!", "//", "http:", "https:", "빈이", "비니", "빈", "최빈", "채빈", "봇", "삐뽀", "이모티콘을 보냈습니다.", "!운세", "/", "|", ".", "관리자", "오리방", "지구방", "니셜이", "니셔리", "니셔", "?", "체온", "체오니"]; 
+var noSave = ["사진을 보냈습니다.", "동영상을 보냇습니다.", "음성메시지를 보냈습니다.", "카카오톡 프로필", "www.", "샵검색:", "#", "@", "!", "//", "http:", "https:", "빈이", "비니", "빈", "최빈", "채빈", "봇", "삐뽀", "이모티콘을 보냈습니다.", "!운세", "/", "|", ".", "관리자", "오리방", "지구방", "니셜이", "니셔리", "니셔", "체온", "체오니", "필터링"]; 
 for (var n = 0; n < noSave.length; n++) { 
 if (data.startsWith(noSave[n])) return false;
 }
-noSave = ["\n", "/", "이모티콘", "사진", "음성메시지", "보이스톡", "페이스톡", "동영상", "카카오톡", "#", "//", "!운세", "삐뽀", "최빈", "체온", "뾰롱", "리보", "가현", "봇", "학습", "빈", "비니", "가혀", "빈은", "최빈이는", "빈이는", "채빈", "채비니", "체빈", "DB", "운세", "/", "관리자", "보노", "지구방", "니셜이", "니셔리", "니셔", "땃쥐", "뱁새", "뱁새야", "뱁새봇", "최빈봇", "버전", "업데이트", "DB", "친추", "정지", "-", "씨바", "븅신", "병신", "임티", "운세는", "@", "방장", "운영", "관리", "부방", "!!", "스샷", "캡처",  "(", "씨발", "수동", "자동", ")", "{", "}", "니스", "sat", "SAT", "최빙", "체빈", "채빈", "닉변", "공지", "깃헙", "내역", "*", "패치", "베타", "업데이트", "닉변", "공지", "게시판", "투표", "펫", "!타이머", "타이머", "!한강물", "한강물", "!주사위", "주사위", "github"];
+noSave = ["\n", "/", "이모티콘", "사진", "음성메시지", "보이스톡", "페이스톡", "동영상", "카카오톡", "#", "//", "!운세", "삐뽀", "최빈", "체온", "뾰롱", "리보", "가현", "봇", "학습", "빈", "비니", "가혀", "빈은", "최빈이는", "빈이는", "채빈", "채비니", "체빈", "DB", "운세", "/", "관리자", "보노", "지구방", "니셜이", "니셔리", "니셔", "땃쥐", "뱁새", "뱁새야", "뱁새봇", "최빈봇", "버전", "업데이트", "DB", "친추", "정지", "-", "씨바", "븅신", "병신", "임티", "운세는", "@", "방장", "운영", "관리", "부방", "!!", "스샷", "캡처",  "(", "씨발", "수동", "자동", ")", "{", "}", "니스", "sat", "SAT", "최빙", "체빈", "채빈", "닉변", "공지", "깃헙", "내역", "*", "패치", "베타", "업데이트", "닉변", "공지", "게시판", "투표", "펫", "!타이머", "타이머", "!한강물", "한강물", "!주사위", "주사위", "github", "!필터링", "필터링", "!쌉소리", "쌉소리", "!블랙", "코로나", "확진", "자가 격리", "자가격리"];
 for (var n = 0; n < noSave.length; n++) { 
 if (data.indexOf(noSave[n]) != -1) return false; 
 }
@@ -90,59 +82,7 @@ return "에러: " + e;
 }
 };
 
-DB.loadGHData = function(FileName) {
-try {
-var url = new java.net.URL("https://raw.githubusercontent.com/Rutheon/ChoiB_KKT/main/File/" + FileName + ".txt");
-var con = url.openConnection();
-if (con != null) {
-con.setConnectTimeout(5000);
-con.setUseCaches(false);
-var isr = new java.io.InputStreamReader(con.getInputStream());
-var br = new java.io.BufferedReader(isr);
-var str = br.readLine();
-var line = "";
-while ((line = br.readLine()) != null) {
-str += "\n" + line;
-}
-isr.close();
-br.close();
-con.disconnect();
-}
-return str.toString();
-} catch (e) {
-return "에러: " + e;
-}
-};
-
-DB.initChatData1 = function() {
-var data = DB.loadGHData("B1");
-if (data == null) {
-null;
-} else {
-G1 = data.split("\n");
-}
-};
-
-DB.initChatData2 = function() {
-var data = DB.loadGHData("B2");
-if (data == null) {
-null;
-} else {
-G2 = data.split("\n");
-}
-};
-
-DB.initChatData3 = function() {
-var data = DB.loadGHData("Blist");
-if (data == null) {
-null;
-} else {
-GHBlist = data.split("\n");
-}
-};
-
 DB.createDir();
-DB.initChatData();
 
 function response(room, msg, sender, isGroupChat, replier, ImageDB) {
 msg = msg.trim();
@@ -159,11 +99,6 @@ if (CH.isValidSender(sender) && CH.isValidData(msg)) {
 CH.study(room, msg); 
 }
 
-var noReply = [".", "!", "/"];
-for (var n = 0; n < noReply.length; n++) {
-if (msg.startsWith(noReply[n])) return;
-}
-  
 if (room == "지구b") {
 if ((Math.floor(Math.random() * 5)) == 0) {
 var chat = CH.getRandomChat(room);
@@ -174,19 +109,17 @@ CH.say(chat, replier);
 }
 
 if (msg == "!블랙") {
-var data = GHlist
-var Blist = data.replace(",", "\n");
-CH.say("내가 무시까는 사람들 명단이야.\n해제 요청은 체온 갠톡으로 와!\n\n\n" + Utils.compress() + Blist, replier);
+var Blist0 = ["방장봇", "봇", ".", "ㅎㅇ"];
+var Blist = Blist0.replace(",", "\n");
+CH.say("내가 무시까는 사람들 명단이야.\n해제 요청은 체온 갠톡으로 와!" + Utils.compress() + "\n\n\n아래 단어가 닉넴에 포함시 무시:\n" + Blist, replier);
 }
-  
+
 if (msg == "!필터링") {
-var data1 = G1
-var FTlist1 = data1.replace(",", "\n");
-var data2 = G2
-var FTlist2 = data2.replace(",", "\n");
-CH.say("내가 배우지 않는 단어 목록들이야.\n\n\n" + Utils.compress() + "해당 단어로 시작하는 챗 무시:\n" + FTlist1 + "\n\n\n해당 단어 포함시 무시:\n" + FTlist2,replier);
+var List0 = ["(엔터 포함된 채팅)", "/", "이모티콘", "사진", "음성메시지", "보이스톡", "페이스톡", "동영상", "카카오톡", "#", "//", "!운세", "삐뽀", "최빈", "체온", "뾰롱", "리보", "가현", "봇", "학습", "빈", "비니", "가혀", "빈은", "최빈이는", "빈이는", "채빈", "채비니", "체빈", "DB", "운세", "/", "관리자", "보노", "지구방", "니셜이", "니셔리", "니셔", "땃쥐", "뱁새", "뱁새야", "뱁새봇", "최빈봇", "버전", "업데이트", "DB", "친추", "정지", "-", "씨바", "븅신", "병신", "임티", "운세는", "@", "방장", "운영", "관리", "부방", "!!", "스샷", "캡처",  "(", "씨발", "수동", "자동", ")", "{", "}", "니스", "sat", "SAT", "최빙", "체빈", "채빈", "닉변", "공지", "깃헙", "내역", "*", "패치", "베타", "업데이트", "닉변", "공지", "게시판", "투표", "펫", "!타이머", "타이머", "!한강물", "한강물", "!주사위", "주사위", "github", "!필터링", "필터링", "!쌉소리", "쌉소리", "!블랙", "코로나", "확진", "자가 격리", "자가격리"];
+var List = List0.replace(",", "\n");
+CH.say("지금 내가 안배우는 단어 목록들이야." + Utils.compress() + "\n\n\n아래 단어가 채팅에 포함시 무시:\n" + List, replier);
 }
-  
+
 if (msg == "!쌉소리") {
 var data = DB.readData(room); 
 var datal = data.split("\n").length; 
