597b288b3e8c7d5483aa46b32d95727cc633f1fd
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2020-12-08 08:59:59 -0500

    v1.3

--- 한강물.js
+++ 한강물.js
@@ -1,13 +1,11 @@
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) { 
 
-if (room == "지구b") { 
 if (msg == "!한강물") {
 let data = Utils.getWebText("http://hangang.dkserver.wo.tc");
 let temp = data.split("temp\":\"")[1].split("\"")[0];
 let time = data.split("time\":\"")[1].split("\"")[0];
 replier.reply("현재 한강물의 온도는 " + temp + "°C 인데,\n어때? 입수하실?");
 replier.reply(time);
-}
 }
   
 }
