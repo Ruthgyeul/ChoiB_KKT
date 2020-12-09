94ec85546df55861a768574b3cbcc70ec81f78a0
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2020-12-09 08:30:56 -0500

    v1.3

--- 한강물.js
+++ 한강물.js
@@ -4,7 +4,7 @@ if (msg == "!한강물") {
 let data = Utils.getWebText("http://hangang.dkserver.wo.tc");
 let temp = data.split("temp\":\"")[1].split("\"")[0];
 let time = data.split("time\":\"")[1].split("\"")[0];
-if (Math.floor(Math.random()*16)==0) {
+if (Math.floor(Math.random()*14)==0) {
 replier.reply("엥? 한강물 따위 관심 없는데?");
 } else {
 replier.reply("현재 한강물의 온도는 " + temp + "°C 인데,\n어때? 입수하실?");
