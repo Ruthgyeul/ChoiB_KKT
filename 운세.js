0e5c09a806724474471358a7c14e7755f2075cec
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2020-12-09 00:43:07 -0500

    v1.3

--- 운세.js
+++ 운세.js
@@ -1,9 +1,7 @@
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) { 
 
 if (msg == "!운세") {
-replier.reply("(최빈이가 당신의 운명에 대해 점치기 시작챘다..)");
-java.lang.Thread.sleep(600);
-if (Math.floor(Math.random() * 12) == 0) {
+if (Math.floor(Math.random() * 30) == 0) {
 replier.reply("엥? " + sender + " 친구한테는 보이는게 없어!\n운세가 측정 불가인걸?\n판단은 알어서 해~"); 
 } else {
 let dice = Math.floor(Math.random() * 100) + 0;
