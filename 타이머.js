96f3ca2844e0f2ce15c70c4b7c12719fe786b2ae
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2020-12-09 22:47:43 -0500

    v1.3

--- 타이머.js
+++ 타이머.js
@@ -11,14 +11,10 @@ replier.reply("체온이의 " + num + "초 타이머 끝!\n진동 알림 전송 
 }
 } else {
 if (msg.indexOf("!타이머")==0){
-let mnun = (msg.split(" ")[1]);
-let num = Number(mnum.replace(/[^0-9]/g,""));
-if (mnum != NaN) {
+let num = Number(msg.split(" ")[1].replace(/[^0-9]/g,""));
 replier.reply(sender + " 친구의 타이머 시작!\n" + num + "초 뒤에 타이머가 종료될꺼야~");
 java.lang.Thread.sleep(num*1000);
 replier.reply(sender + "의 " + num + "초가 끝났다구~"); 
-} else if (mnum == NaN) {
-replier.reply("앗! 이해를 하지 못했어!\n!타이머 (숫자) 형식으로 알려줘!");
 }
 }
 }
