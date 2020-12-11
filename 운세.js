9d706d2a63f9623e6eb8437ea8d64325f58c50f5
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2020-12-11 15:48:43 -0500

    Update 운세.js

--- 운세.js
+++ 운세.js
@@ -1,36 +1,36 @@
-//계산: 100(최소 포함/1000) - 100(최대 포함/1000)
-//0% - 0.3%
-//1% ~ 30% - 14.3%
-//31% ~70% - 53%
-//71% ~ 85% - 20%
-//86% ~ 99% - 12.2%
-//100% - 0.2%
+//계산(기준: %): 100(최소 포함/10000) - 100(최대 포함/10000) 
+//0% - 0.4%
+//1% ~ 21% - 4%
+//22% ~86% - 58%
+//87% ~ 94% - 24%
+//95% ~ 99% - 13.5%
+//100% - 0.1%
 
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) { 
 
 if (msg == "!운세") {
-let rand = Math.floor(Math.random() * (1001 - 1)) + 1; //100%
-let dice1 = Math.floor(Math.random() * (30 - 1)) + 1; //14.3%
-let dice2 = Math.floor(Math.random() * (70 - 31)) + 31; //53%
-let dice3 = Math.floor(Math.random() * (85 - 71)) + 71; //20%
-let dice4 = Math.floor(Math.random() * (99 - 86)) + 86; //12.2%
+let rand = Math.floor(Math.random() * (10000 - 1)) + 1; //100%
+let dice1 = Math.floor(Math.random() * (21 - 1)) + 1; //4%
+let dice2 = Math.floor(Math.random() * (86 - 22)) + 22; //58%
+let dice3 = Math.floor(Math.random() * (94 - 87)) + 87; //24%
+let dice4 = Math.floor(Math.random() * (99 - 95)) + 95; //13.5%
 
-if (rand < 4) {
+if (rand < 11) {
 replier.reply((Math.floor(Math.random() * 2) ? "헐! " : "아? ") + sender + " 친구의 운세는 무려 100%야!!!");
 } 
-if (rand > 3 && rand < 8) {
+if (rand > 10 && rand < 51) {
 replier.reply((Math.floor(Math.random() * 2) ? "엌ㅋ! " : "아.. ") + sender + " 친구의 운세는 무려 0%라고~ㅋ");
 }
-if (rand > 7 && rand < 131) {
+if (rand > 50 && rand < 1401) {
 replier.reply((Math.floor(Math.random() * 2) ? "앗! " : "우왕! ") + sender + " 친구의 운세는 " + dice4 + "%가 나왔엉~d4");
 }
-if (rand > 130 && rand < 275) {
+if (rand > 1400 && rand < 1441) {
 replier.reply((Math.floor(Math.random() * 2) ? "앗! " : "우왕! ") + sender + " 친구의 운세는 " + dice1 + "%가 나왔엉~d1");
 }
-if (rand > 274 && rand < 476) {
+if (rand > 1440 && rand < 3801) {
 replier.reply((Math.floor(Math.random() * 2) ? "앗! " : "우왕! ") + sender + " 친구의 운세는 " + dice3 + "%가 나왔엉~d3");
 }
-if (rand > 475) {
+if (rand > 3800) {
 replier.reply((Math.floor(Math.random() * 2) ? "앗! " : "우왕! ") + sender + " 친구의 운세는 " + dice2 + "%가 나왔엉~d2");
 }
   
