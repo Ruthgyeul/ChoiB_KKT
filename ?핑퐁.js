512c25fe2aa3eac042d8e8ed7e991f742ad21e9d
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2020-12-15 10:40:06 -0500

    BETA v1.4.1

--- ?핑퐁.js
+++ ?핑퐁.js
@@ -1,10 +1,10 @@
 function send() {
 try {
-let url = new java.net.URL("https://builder.pingpong.us/api/builder/5e1a1c75e4b010b663d37764/integration/v0.2/custom/{sessionId}");
+let url = new java.net.URL("https://builder.pingpong.us/api/builder/5fd8cb7ae4b07b8420aa1ed4/integration/v0.2/custom/{sessionId}");
 let con = url.openConnection();
 con.setRequestMethod("POST"); // 서버 접속 방법: GET, POST, OPTIONS 등..
 con.setRequestProperty("Content-Type", "application/json; charset=utf-8"); // 서버 접속시 가져올 데이터의 형식
-con.setRequestProperty("Authorization", "Basic " + "a2V5OjBlMDY2ZjJlZTUxNmZhY2JmZDFmMGQyMThmMDJkZjkz"); // 인증키 입력
+con.setRequestProperty("Authorization", "Basic " + "a2V5OmNjMGE2MDcyM2UyZTI3NzA2MGQ4YzkyYWVhZjhiZGQ1"); // 인증키 입력
 con.setRequestProperty("User-Agent", "Mozilla"); 
 con.setRequestProperty("Accpet", "*.*");
 con.setDoOutput(true);
@@ -36,7 +36,19 @@ replier.reply(results["response"]["replies"][0]["text"]);
 }
 
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) { 
-if (msg.indexOf(".")==0) {
+
+if (msg.indexOf("최빈아")==0 || msg.indexOf("빈아")==0) {
 msg = msg.replace(/./,"");
 let jsondata = {"request":{"query": msg}};
+}
+  
+if (msg) {
+if (Math.floor(Math.random() * 10) == 0) {
+msg = msg.replace(/./,"");
+let jsondata = {"request":{"query": msg}};
+} else {
+null;
+}
+}
+  
 }
