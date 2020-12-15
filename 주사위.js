f3dc8dbeb6dddb6fbbf01c6b3798dfba2a229fb0
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2020-12-15 08:31:12 -0500

    v1.4.1

--- 주사위.js
+++ 주사위.js
@@ -1,5 +1,4 @@
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) { 
-
 if (msg == "!주사위") {
 if (Math.floor(Math.random() * 12) == 0) {
 replier.reply("엇! " + sender + "의 주사위가 시아를 벋어났어!\n주사위를 좀 더 살살 다뤄줘~!"); 
@@ -8,5 +7,4 @@ let dice = Math.floor(Math.random() * 6) + 1;
 replier.reply((Math.floor(Math.random() * 2) ? "앗! " : "우왕! ") + sender + " 친구의 주사위에서 「" + dice + (([1, 3, 6].indexOf(dice) != -1) ? "」이" : "」가") + " 나왔어~");
 }
 }
-  
 }
