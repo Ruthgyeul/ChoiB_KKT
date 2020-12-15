14ec1fd3c094d85d3918b1afbb9f551e8c54e6cd
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2020-12-15 11:37:27 -0500

    BETA v1.4.1

--- ?핑퐁.js
+++ ?핑퐁.js
@@ -1,56 +1,65 @@
+const AuthorizationID = "a2V5OmNjMGE2MDcyM2UyZTI3NzA2MGQ4YzkyYWVhZjhiZGQ1";
+const CH = {};
+
+CH.say = function(msg, replier) {
+replier.reply("[PP] " + msg);
+}
+
+CH.send = function(msg) {
+try {
+let url = new java.net.URL("https://builder.pingpong.us/api/builder/5e1a1c75e4b010b663d37764/integration/v0.2/custom/{sessionId}");
+let con = url.openConnection();
+con.setRequestMethod("POST"); 
+con.setRequestProperty("Content-Type", "application/json; charset=utf-8"); 
+con.setRequestProperty("Authorization", "Basic " + "a2V5OmNjMGE2MDcyM2UyZTI3NzA2MGQ4YzkyYWVhZjhiZGQ1"); 
+con.setRequestProperty("User-Agent", "Mozilla");
+con.setRequestProperty("Accpet", "*.*"); 
+con.setDoOutput(true);
+let jsondata = { "request": { "query": msg } };
+let wr = new java.io.DataOutputStream(con.getOutputStream());
+let writer = new java.io.BufferedWriter(new java.io.OutputStreamWriter(wr, "UTF-8"));
+writer.write(JSON.stringify(jsondata));
+writer.close();
+wr.close();
+let responseCode = con.getResponseCode();
+let br;
+if (responseCode == 200) {
+br = new java.io.BufferedReader(new java.io.InputStreamReader(con.getInputStream()));
+} else {
+br = new java.io.BufferedReader(new java.io.InputStreamReader(con.getErrorStream()));
+}
+let inputLine;
+let response = "";
+while ((inputLine = br.readLine()) != null) {
+response += inputLine;
+}
+br.close();
+return response;
+} catch (e) {
+return "에러: " + e;
+}
+}
+
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) { 
 
-  if (msg.indexOf(".") == 0) {
-    msg = msg.replace(/./,"");
-    let jsondata = { "request": { "query": msg } };
+if (msg.startsWith("최빈아")==0) {
+msg = msg.replace(/최빈아/,"");
+let results = JSON.parse(CH.send(msg));
+CH.say(results["response"]["replies"][0]["text"],replier);
+}
 
-    function send() {
-      try {
-        let url = new java.net.URL("https://builder.pingpong.us/api/builder/5e1a1c75e4b010b663d37764/integration/v0.2/custom/{sessionId}");
-        let con = url.openConnection();
-        con.setRequestMethod("POST"); // 서버 접속 방법을 설정하세요. GET, POST, OPTIONS 등..
-        con.setRequestProperty("Content-Type", "application/json; charset=utf-8"); // 서버 접속시 가져올 데이터의 형식을 지정
-        con.setRequestProperty("Authorization", "Basic " + "a2V5OmNjMGE2MDcyM2UyZTI3NzA2MGQ4YzkyYWVhZjhiZGQ1"); // 인증키 입력. 사이트에 따라 Basic 또는 Bearer 를 사용합니다.
-        con.setRequestProperty("User-Agent", "Mozilla"); // 일부 사이트의 경우 User-Agent 를 요구합니다.
-        con.setRequestProperty("Accpet", "*.*"); // 일부 사이트의 경우, 이 헤더가 없으면 오류가 발생합니다.
-        con.setDoOutput(true);
-        let wr = new java.io.DataOutputStream(con.getOutputStream());
-        let writer = new java.io.BufferedWriter(new java.io.OutputStreamWriter(wr, "UTF-8"));
-        writer.write(JSON.stringify(jsondata));
-        writer.close();
-        wr.close();
-
-        let responseCode = con.getResponseCode();
-        let br;
-        if (responseCode == 200) {
-          br = new java.io.BufferedReader(new java.io.InputStreamReader(con.getInputStream()));
-        } else {
-          br = new java.io.BufferedReader(new java.io.InputStreamReader(con.getErrorStream()));
-        }
-        let inputLine;
-        let response = "";
-        while ((inputLine = br.readLine()) != null) {
-          response += inputLine;
-        }
-        br.close();
-        return response;
-      } catch (e) {
-        return e;
-      }
-    }
-
-
-    let results = JSON.parse(send());
-    replier.reply(results['response']['replies'][0]['text']);
-  }
- /* 
-if (msg) {
-if (Math.floor(Math.random() * 10) == 0) {
-msg = msg.replace(/./,"");
-let jsondata = {"request":{"query": msg}};
-} else {
-null;
+if (room == "지구b") {
+if ((Math.floor(Math.random() * 5)) == 0) {
+let results = JSON.parse(CH.send(msg));
+CH.say(results["response"]["replies"][0]["text"],replier);
 }
 }
-  */
+  
+if (room == "오리b") {
+if ((Math.floor(Math.random() * 6)) == 0) {
+let results = JSON.parse(CH.send(msg));
+CH.say(results["response"]["replies"][0]["text"],replier);
+}
+}
+  
 }
