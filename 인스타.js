f1d14d85ce72d2be44ed7d8e7a38b32cc87b0e2c
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2020-12-14 23:10:53 -0500

    BETA

--- 인스타.js
+++ 인스타.js
@@ -1,20 +1,20 @@
 const Jsoup = org.jsoup.Jsoup;
-const Lw = '\u200b'.repeat(1000);
-const Ll = '⎼'.repeat(50);
+const Lw = "\u200b".repeat(1000);
+const Ll = "⎼".repeat(50);
 
 function response (room, msg, sender, igc, replier) {
     
 if (msg.startsWith("!인스타")) {
 try {
 let nick = msg.substring(4).trim();
-let parse = Jsoup.connect('https://www.instagram.com/'+nick).get().html().split('_sharedData =')[1].split('<')[0].trim();
+let parse = Jsoup.connect("https://www.instagram.com/" + nick).get().html().split("_sharedData =")[1].split("<")[0].trim();
 let json = JSON.parse(parse.slice(0, parse.length-1));
 let info = json.entry_data.ProfilePage[0].graphql.user;
 with (info) {
 let articles = edge_owner_to_timeline_media.edges.map(e => {
 let info2 = e.node;
 let text = info2.edge_media_to_caption.edges[0]
-text = text ? text.node.text : '설명글 없음.';
+text = text ? text.node.text : "설명글 없음.";
 let url = info2.display_url;
 let likes = info2.edge_liked_by.count;
 let comments = info2.edge_media_to_comment.count;
