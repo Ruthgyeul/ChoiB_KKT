09bf91454981a597dcf6a914056a90d89ce5bc70
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2020-12-14 23:20:48 -0500

    Update 맞춤법.js

--- 맞춤법.js
+++ 맞춤법.js
@@ -1,24 +1,25 @@
-// 맞춤법 꼽주기
-  if (msg.indexOf("됬") !== -1) {
-    var toReply = ""
-    for (i = 0; i < 36; i++) {
-      toReply += "됐"
-    }
-    replier.reply(toReply);
-  }
 
-  if (msg.indexOf("됌") !== -1) {
-    var toReply = ""
-    for (i = 0; i < 36; i++) {
-      toReply += "됨"
-    }
-    replier.reply(toReply);
-  }
 
-  if (msg.indexOf("됀") !== -1) {
-    var toReply = ""
-    for (i = 0; i < 36; i++) {
-      toReply += "된"
-    }
-    replier.reply(toReply);
-  }
+if (msg.indexOf("됬")==0) {
+var toReply = ""
+for (i = 0; i < 36; i++) {
+toReply += "됐"
+}
+CH.say(toReply,replier);
+}
+
+if (msg.indexOf("됌")==0) {
+var toReply = ""
+for (i = 0; i < 36; i++) {
+toReply += "됨"
+}
+CH.say(toReply,replier);
+}
+
+if (msg.indexOf("됀")==0) {
+var toReply = ""
+for (i = 0; i < 36; i++) {
+toReply += "된"
+}
+CH.say(toReply,replier);
+}
