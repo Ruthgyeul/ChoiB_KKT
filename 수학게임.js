3c3aad5b2bc5a97037fbb8ece971a72ae3428908
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2020-12-16 12:55:43 -0500

    v1.4.1

--- 수학게임.js
+++ 수학게임.js
@@ -5,19 +5,69 @@ var answerForCalcQuiz = null;
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
 
 if (msg == "!수학게임") {
+replier.reply("스테이지를 선택해주세요!\n\n!수학게임1 = n + n\n!수학게임2 = n - n\n!수학게임3 = n × n\n!수학게임4 = n / n\n!수학게임5 = ???");
+}
+
+if (msg == "!수학게임1") {
 if (msgSenderForCalcQuiz == null) {
 msgSenderForCalcQuiz = sender;
 var Num1 = Math.floor(Math.random() * 100 + 1);
 var Num2 = Math.floor(Math.random() * 100 + 1);
-var calcItem = ["+", "+", "+", "×", "×"]
-var calcItemRandom = Math.floor(Math.random() * 5);
-var calcItemToUse = calcItem[calcItemRandom];
-if (calcItemRandom <= 3) {
 answerForCalcQuiz = Num1 + Num2;
+replier.reply("문제: " + Num1 + " + " + Num2 + " = ?\n\n명령어: !정답 (숫자)");
+getCalcQuizOn = 1;
 } else {
-answerForCalcQuiz = Num1 * Num2;
+replier.reply("현재 진행 중인 게임이 있습니다.\n초기화 명령어: !수학게임초기화");
 }
-replier.reply("문제: " + Num1 + " " + calcItemToUse + " " + Num2 + " = ?\n\n명령어: !정답 (숫자)");
+}
+  
+if (msg == "!수학게임2") {
+if (msgSenderForCalcQuiz == null) {
+msgSenderForCalcQuiz = sender;
+var Num1 = Math.floor(Math.random() * 100 + 1);
+var Num2 = Math.floor(Math.random() * 10 + 1);
+answerForCalcQuiz = Num1 - Num2;
+replier.reply("문제: " + Num1 + " - " + Num2 + " = ?\n\n명령어: !정답 (숫자)");
+getCalcQuizOn = 1;
+} else {
+replier.reply("현재 진행 중인 게임이 있습니다.\n초기화 명령어: !수학게임초기화");
+}
+}
+  
+if (msg == "!수학게임3") {
+if (msgSenderForCalcQuiz == null) {
+msgSenderForCalcQuiz = sender;
+var Num1 = Math.floor(Math.random() * 100 + 1);
+var Num2 = Math.floor(Math.random() * 10 + 1);
+answerForCalcQuiz = Num1 * Num2;
+replier.reply("문제: " + Num1 + " × " + Num2 + " = ?\n\n명령어: !정답 (숫자)");
+getCalcQuizOn = 1;
+} else {
+replier.reply("현재 진행 중인 게임이 있습니다.\n초기화 명령어: !수학게임초기화");
+}
+}
+  
+if (msg == "!수학게임4") {
+if (msgSenderForCalcQuiz == null) {
+msgSenderForCalcQuiz = sender;
+var Num1 = Math.floor(Math.random() * 100 + 1);
+var Num2 = Math.floor(Math.random() * 10 + 1);
+answerForCalcQuiz = Num1 / Num2;
+replier.reply("문제: " + Num1 + " / " + Num2 + " = ?\n\n명령어: !정답 (숫자)");
+getCalcQuizOn = 1;
+} else {
+replier.reply("현재 진행 중인 게임이 있습니다.\n초기화 명령어: !수학게임초기화");
+}
+}
+  
+if (msg == "!수학게임5") {
+if (msgSenderForCalcQuiz == null) {
+msgSenderForCalcQuiz = sender;
+var Num1 = Math.floor(Math.random() * 100 + 1);
+var Num2 = Math.floor(Math.random() * 10 + 1);
+var Num3 = Math.floor(Math.random() * 10 + 1);
+answerForCalcQuiz = Num1 + (Num2 * Num3);
+replier.reply("문제: " + Num1 + " + " + Num2 + " × " + Num3 + " = ?\n\n명령어: !정답 (숫자)");
 getCalcQuizOn = 1;
 } else {
 replier.reply("현재 진행 중인 게임이 있습니다.\n초기화 명령어: !수학게임초기화");
