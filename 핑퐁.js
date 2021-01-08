a6477d8a00b9df5e56dcfdf29bc418926f28eba4
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2021-01-08 08:30:04 -0600

    Update 핑퐁.js

--- 핑퐁.js
+++ 핑퐁.js
@@ -1,16 +1,29 @@
 const AuthURL = "https://builder.pingpong.us/api/builder/5fd8cb7ae4b07b8420aa1ed4/integration/v0.2/custom/{sessionId}";
 const AuthID = "Basic a2V5OmNjMGE2MDcyM2UyZTI3NzA2MGQ4YzkyYWVhZjhiZGQ1";
+const CH = {};
 
-function send(Idata) {
+CH.say = function(msg, replier) {
+replier.reply(msg);
+}
+
+CH.rsay = function(R, msg, replier) {
+if (Math.floor(Math.random() * Number(R)) == 0) {
+replier.reply(msg);
+} else {
+null;
+}
+}
+
+CH.send = function(Idata) {
 try {
-let jsondata = { "request": { "query": Idata } }; 
-let url = new java.net.URL("https://builder.pingpong.us/api/builder/5e1a1c75e4b010b663d37764/integration/v0.2/custom/{sessionId}");
+let jsondata = { "request": { "query": Idata } }; //서버로 전송하는 메시지
+let url = new java.net.URL("https://builder.pingpong.us/api/builder/5e1a1c75e4b010b663d37764/integration/v0.2/custom/{sessionId}"); //AuthURL
 let con = url.openConnection();
-con.setRequestMethod("POST"); // 서버 접속 방법을 설정하세요. GET, POST, OPTIONS 등..
-con.setRequestProperty("Content-Type", "application/json; charset=utf-8"); // 서버 접속시 가져올 데이터의 형식
-con.setRequestProperty("Authorization", "Basic " + "a2V5OmNjMGE2MDcyM2UyZTI3NzA2MGQ4YzkyYWVhZjhiZGQ1"); // 인증키 입력. 사이트에 따라 Basic 또는 Bearer 를 사용합니다.
-con.setRequestProperty("User-Agent", "Mozilla"); // 일부 사이트의 경우 User-Agent 를 요구합니다.
-con.setRequestProperty("Accpet", "*.*"); // 일부 사이트의 경우, 이 헤더가 없으면 오류가 발생합니다.
+con.setRequestMethod("POST"); //GET, POST, OPTIONS
+con.setRequestProperty("Content-Type", "application/json; charset=utf-8"); //서버 접속시 가져올 데이터 형식
+con.setRequestProperty("Authorization", "Basic " + "a2V5OmNjMGE2MDcyM2UyZTI3NzA2MGQ4YzkyYWVhZjhiZGQ1"); //AuthID
+con.setRequestProperty("User-Agent", "Mozilla"); //일부 사이트의 경우 User-Agent를 요구
+con.setRequestProperty("Accpet", "*.*"); //일부 사이트의 경우 이 헤더가 없으면 오류가 발생
 con.setDoOutput(true);
 let wr = new java.io.DataOutputStream(con.getOutputStream());
 let writer = new java.io.BufferedWriter(new java.io.OutputStreamWriter(wr, "UTF-8"));
@@ -35,51 +48,51 @@ return response;
 } catch (e) {
 return e;
 }
-}
+};
 
-function isValidMsg(msg) {
+CH.isValidMsg = function(Data) {
 var noValid = [".", "최빈", "최비", "빈", "비니", "/", "!", "이모티콘을 보냈습니다.", "이모티콘을", "사진을 보냈습니다.", "사진을", "동영상을 보냈습니다.", "동영상을", "음성메시지를 보냈습니다.", "음성메시지", "카카오톡 프로필", "샵검색:", "#", "@", "www.", "http:", "https:", ".com", ".gov", ".kr", ".org", "보이스톡", "페이스톡"]; 
 for (var n = 0; n < noValid.length; n++) { 
-if (msg.startsWith(noValid[n]) != -1) return false; 
+if (Data.startsWith(noValid[n]) != -1) return false; 
 }
 return true;
-}
+};
 
-function isValidSys(msg) {
+CH.isValidSys = function(Data) {
 var noValid = ["이모티콘을 보냈습니다.", "이모티콘을", "사진을 보냈습니다.", "사진을", "동영상을 보냈습니다.", "동영상을", "음성메시지를 보냈습니다.", "음성메시지", "카카오톡 프로필", "샵검색:", "#", "@", "www.", "http:", "https:", ".com", ".gov", ".kr", ".org", "보이스톡", "페이스톡"]; 
 for (var n = 0; n < noValid.length; n++) { 
-if (msg.startsWith(noValid[n]) != -1) return false; 
+if (Data.startsWith(noValid[n]) != -1) return false; 
 }
 return true;
-}
+};
 
-function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) { 
+function response(room, msg, sender, isGroupChat, replier) { 
 
 if (room.indexOf("b")) {
 if (msg.startsWith(".") || msg.startsWith("최빈") || msg.startsWith("최비") || msg.startsWith("빈") || msg.startsWith("비니")) {
 msg = msg.replace(/./,"");
-let results = JSON.parse(send(msg));
-replier.reply(results['response']['replies'][0]['text']);
+let results = JSON.parse(CH.send(msg));
+CH.say(results['response']['replies'][0]['text'],replier);
 } 
 }
 
-if (isGroupChat == false && isValidSys(msg)) {
-let results = JSON.parse(send(msg));
-replier.reply(results['response']['replies'][0]['text']);
+if (isGroupChat == false && CH.isValidSys(msg)) {
+let results = JSON.parse(CH.send(msg));
+CH.say(results['response']['replies'][0]['text'],replier);
 }
   
-if (room == "지구b" && isValidMsg(msg)) {
-let results = JSON.parse(send(msg));
-if (Math.floor(Math.random() * 4) == 0) {
-replier.reply(results['response']['replies'][0]['text']);
-}
+if (room == "지구b" && CH.isValidMsg(msg)) {
+let results = JSON.parse(CH.send(msg));
+CH.rsay(4, results['response']['replies'][0]['text'],replier);
 }
   
-if (room == "삐약b" && isValidMsg(msg)) {
-let results = JSON.parse(send(msg));
-if (Math.floor(Math.random() * 5) == 0) {
-replier.reply(results['response']['replies'][0]['text']);
+if (room == "삐약b" && CH.isValidMsg(msg)) {
+let results = JSON.parse(CH.send(msg));
+CH.rsay(5, results['response']['replies'][0]['text'],replier);
 }
+  
+if (sender.indexOf("체온") && msg == "!테스트P") {
+CH.say("[PingPong API]\n\n" + CH.send("테스트"),replier);
 }
   
 }
