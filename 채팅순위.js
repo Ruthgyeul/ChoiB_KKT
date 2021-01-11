c148d9f1a132fb7debdc3dfc669fefeb5c963abe
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2021-01-11 09:22:52 -0600

    Create 채팅순위.js

---
+++ 채팅순위.js
@@ -0,0 +1,23 @@
+const FS = FileStream, path = '/sdcard/chat.json';
+if(!new java.io.File(path).canRead()) FS.write(path, '[]');
+let chat = JSON.parse(FS.read(path));
+
+function response(room, msg, sender, igc, replier){
+    
+    if(!chat.find(e=>e.name==sender)) chat.push({'name':sender, 'count':1});
+    chat[chat.findIndex(e=>e.name==sender)].count++;
+
+    if(msg=='!채팅순위'){
+        let total = chat.map(e=>e.count).reduce((a,b)=>a+b);
+        replier.reply('『 🗣️ Chat Rank 』'+'\u200b'.repeat(500)+'\nTotal : '+total+'\n'+'═'.repeat(20)+'\n\n'+chat.sort((a,b)=>b.count-a.count).slice(0,150).map((e,i)=>++i+'위 ['+e.count+'회, '+(e.count/total*100).toFixed(2)+'%] : '+e.name).join('\n\n')+'\n\n'+'═'.repeat(20));
+    }
+
+    if(msg=='!내 채팅'){
+        let index = chat.sort((a,b)=>b.count-a.count).findIndex(e=>e.name==sender);
+        replier.reply('『 🗣️ Chat Info 』 '+sender+'\n'+'═'.repeat(12)+'\n『🏅』 순위 : '+(index+1)+'위\n『🏅』 채팅수 : '+chat[index].count+'회\n'+'═'.repeat(12));
+    }
+
+    FS.write(path, JSON.stringify(chat));
+
+}
+//[출처] 채팅순위 (카카오톡 봇 커뮤니티) | 작성자 Jwon0423
