7e4a259a41628c79a8aec6a4bcc21110fa6a5ef7
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2021-01-11 20:15:08 -0600

    Update 핑퐁.js

--- 핑퐁.js
+++ 핑퐁.js
@@ -1,177 +1,15 @@
 const AuthURL = "https://builder.pingpong.us/api/builder/5fd8cb7ae4b07b8420aa1ed4/integration/v0.2/custom/{sessionId}";
 const AuthID = "Basic a2V5OmNjMGE2MDcyM2UyZTI3NzA2MGQ4YzkyYWVhZjhiZGQ1";
+const CH = {};
 const On = {};
 const Pro = {};
 
-function response(room, msg, sender, isGroupChat, replier) { 
-
-if (!On[room]) On[room] = false;
-if (!Pro[room]) Pro[room] = 0;
-
-if (msg == "!Talk On") {
-On[room] = true;
-replier.reply("[Bot]\nRoom : " + room + "\nTalk : On");
-return;
+CH.Admin = function(sender) {
+var Admin = ["체온", "가현", "니셜", "리보", "땃쥐"]; 
+for (var n = 0; n < Admin.length; n++) { 
+if (sender.indexOf(Admin[n]) != -1) return true; 
 }
-
-if (!On[room]) return;
-
-if (msg == "!Talk Off") {
-On[room] = false;
-replier.reply("[Bot]\nRoom : " + room + "\nTalk : Off");
-return;
-}
-
-if (msg.startsWith("!Talk ")) {
-msg = msg.replace("!Talk ", "");
-if (isNaN(msg)) return;
-if (msg < 0 || msg > 100) return;
-Pro[room] = msg;
-replier.reply("[Bot]\nRoom : " + room + "\nProbability : " + msg + "%");
-return;
-}
-
-if (Math.random() * 100 << 0 > (99 - Pro[room])) {
-User = User.replace("Basic ", "");
-let jsondata = { "request": { "query": msg } };
-
-send = _ => {
-
-try{
-
-let url = new java.net.URL(Url);
-
-let con = url.openConnection();
-
-con.setRequestMethod("POST"); // 서버 접속 방법을 설정하세요. GET, POST, OPTIONS 등..
-
-con.setRequestProperty("Content-Type", "application/json; charset=utf-8"); // 서버 접속시 가져올 데이터의 형식을 지정
-
-con.setRequestProperty("Authorization", "Basic " + "a2V5OmNjMGE2MDcyM2UyZTI3NzA2MGQ4YzkyYWVhZjhiZGQ1"); // 인증키 입력. 사이트에 따라 Basic 또는 Bearer 를 사용합니다.
-
-con.setRequestProperty("User-Agent", "Mozilla"); // 일부 사이트의 경우 User-Agent 를 요구합니다.
-
-con.setRequestProperty("Accpet", "*.*"); // 일부 사이트의 경우, 이 헤더가 없으면 오류가 발생합니다.
-
-con.setDoOutput(true);
-
-let wr = new java.io.DataOutputStream(con.getOutputStream());
-
-let writer = new java.io.BufferedWriter(new java.io.OutputStreamWriter(wr, "UTF-8"));
-
-writer.write(JSON.stringify(jsondata));
-
-writer.close();
-
-wr.close();
-
-let responseCode = con.getResponseCode();
-
-let br;
-
-if (responseCode == 200) {
-
-br = new java.io.BufferedReader(new java.io.InputStreamReader(con.getInputStream()));
-
-}
-
-else
-
-br = new java.io.BufferedReader(new java.io.InputStreamReader(con.getErrorStream()));
-
-let inputLine;
-
-let response = "";
-
-while ((inputLine = br.readLine()) != null) {
-
-response += inputLine;
-
-}
-
-br.close();
-
-return response;
-
-} catch (e) {
-
-return e;
-
-}
-
-}
-
-let results = JSON.parse(send());
-
-replier.reply(results["response"]["replies"][0]["text"]);
-
-}
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-CH.say = function(msg, replier) {
-let RData = CH.send(msg);
-RData.replace(/\n/gi, "\n");
-RData.replace(/\r/gi, "\r");
-let results = JSON.parse(RData);
-replier.reply(results['response']['replies'][0]['text']);
-};
-
-CH.rsay = function(R, msg, replier) {
-let RData = CH.send(msg);
-RData.replace(/\n/gi, "\n");
-RData.replace(/\r/gi, "\r");
-let results = JSON.parse(RData);
-if (Math.floor(Math.random() * Number(R)) == 0) {
-replier.reply(results['response']['replies'][0]['text']);
-}
-};
-
-CH.send = function(Idata) {
-try {
-let jsondata = { "request": { "query": Idata } }; //서버로 전송하는 메시지
-let url = new java.net.URL("https://builder.pingpong.us/api/builder/5e1a1c75e4b010b663d37764/integration/v0.2/custom/{sessionId}"); //AuthURL
-let con = url.openConnection();
-con.setRequestMethod("POST"); //GET, POST, OPTIONS
-con.setRequestProperty("Content-Type", "application/json; charset=utf-8"); //서버 접속시 가져올 데이터 형식
-con.setRequestProperty("Authorization", "Basic " + "a2V5OmNjMGE2MDcyM2UyZTI3NzA2MGQ4YzkyYWVhZjhiZGQ1"); //AuthID
-con.setRequestProperty("User-Agent", "Mozilla"); //일부 사이트의 경우 User-Agent를 요구
-con.setRequestProperty("Accpet", "*.*"); //일부 사이트의 경우 이 헤더가 없으면 오류가 발생
-con.setDoOutput(true);
-let wr = new java.io.DataOutputStream(con.getOutputStream());
-let writer = new java.io.BufferedWriter(new java.io.OutputStreamWriter(wr, "UTF-8"));
-writer.write(JSON.stringify(jsondata));
-writer.close();
-wr.close();
-  
-let responseCode = con.getResponseCode();
-let br;
-if (responseCode == 200) {
-br = new java.io.BufferedReader(new java.io.InputStreamReader(con.getInputStream()));
-} else {
-br = new java.io.BufferedReader(new java.io.InputStreamReader(con.getErrorStream()));
-}
-let inputLine;
-let response = "";
-while ((inputLine = br.readLine()) != null) {
-response += inputLine;
-}
-br.close();
-return response;
-} catch (e) {
-return e;
-}
+return false;
 };
 
 CH.isValidMsg = function(Data) {
@@ -182,37 +20,126 @@ if (Data.startsWith(noValid[n]) != -1) return false;
 return true;
 };
 
-CH.isValidSys = function(Data) {
-var noValid = ["이모티콘을 보냈습니다.", "이모티콘을", "사진을 보냈습니다.", "사진을", "동영상을 보냈습니다.", "동영상을", "음성메시지를 보냈습니다.", "음성메시지", "카카오톡 프로필", "샵검색:", "#", "@", "www.", "http:", "https:", ".com", ".gov", ".kr", ".org", "보이스톡", "페이스톡"]; 
-for (var n = 0; n < noValid.length; n++) { 
-if (Data.startsWith(noValid[n]) != -1) return false; 
-}
-return true;
-};
-
 function response(room, msg, sender, isGroupChat, replier) { 
+
+if (!On[room]) On[room] = false;
+if (!Pro[room]) Pro[room] = 0;
+
+if (msg == "!Talk On") {
+if (CH.Admin(sender)) {
+On[room] = true;
+replier.reply("[Project K]\nRoom : " + room + "\nTalk : On");
+return;
+} else if (CH.Admin(sender) == false) {
+replier.reply("[Project K]\n\n관리진 전용 명령어입니다.\n관리진 등록이 되어 있지 않아,\n해당 명령어 실행이 불가합니다.");
+}
+  
+if (!On[room]) return;
+
+if (msg == "!Talk Off") {
+if (CH.Admin(sender)) {
+On[room] = false;
+replier.reply("[Project K]\nRoom : " + room + "\nTalk : Off");
+return;
+} else if (CH.Admin(sender) == false) {
+replier.reply("[Project K]\n\n관리진 전용 명령어입니다.\n관리진 등록이 되어 있지 않아,\n해당 명령어 실행이 불가합니다.");
+}
+
+if (msg.startsWith("!Talk ")) {
+if (CH.Admin(sender)) {
+msg = msg.replace("!Talk ", "");
+if (isNaN(msg)) return;
+if (msg < 0 || msg > 100) return;
+Pro[room] = msg;
+replier.reply("[Project K]\nRoom : " + room + "\nProbability : " + msg + "%");
+return;
+} else if (CH.Admin(sender) == false) {
+replier.reply("[Project K]\n\n관리진 전용 명령어입니다.\n관리진 등록이 되어 있지 않아,\n해당 명령어 실행이 불가합니다.");
+}
+
+if (CH.isValidMsg(msg)) {
+if (Math.random() * 100 << 0 > (99 - Pro[room])) {
+AuthID = AuthID.replace("Basic ", "");
+let jsondata = { "request": { "query": msg } };
+
+send {
+try{
+let url = new java.net.URL(AuthURL);
+let con = url.openConnection();
+con.setRequestMethod("POST"); //서버 접속 방법 설정 GET, POST, OPTIONS
+con.setRequestProperty("Content-Type", "application/json; charset=utf-8"); //서버 연결시 가져올 데이터의 형식
+con.setRequestProperty("Authorization", "Basic " + AuthID); //인증키 입력
+con.setRequestProperty("User-Agent", "Mozilla"); //일부의 경우, User-Agent를 요구
+con.setRequestProperty("Accpet", "*.*"); //일부의 경우, 이 헤더가 없으면 오류가 발생
+con.setDoOutput(true);
+let wr = new java.io.DataOutputStream(con.getOutputStream());
+let writer = new java.io.BufferedWriter(new java.io.OutputStreamWriter(wr, "UTF-8"));
+writer.write(JSON.stringify(jsondata));
+writer.close();
+wr.close();
+let responseCode = con.getResponseCode();
+let br;
+if (responseCode == 200) {
+br = new java.io.BufferedReader(new java.io.InputStreamReader(con.getInputStream()));
+} else {
+br = new java.io.BufferedReader(new java.io.InputStreamReader(con.getErrorStream()));
+let inputLine;
+let response = "";
+while ((inputLine = br.readLine()) != null) {
+response += inputLine;
+}
+br.close();
+return response;
+} catch (e) {
+return e;
+}
+}
+let results = JSON.parse(send());
+replier.reply(results["response"]["replies"][0]["text"]);
+}
+}
 
 if (room.indexOf("b")) {
 if (msg.startsWith(".") || msg.startsWith("최빈") || msg.startsWith("최비") || msg.startsWith("빈") || msg.startsWith("비니")) {
 msg = msg.replace(/./,"");
-CH.say(msg, replier);
-} 
-}
+AuthID = AuthID.replace("Basic ", "");
+let jsondata = { "request": { "query": msg } };
 
-if (isGroupChat == false && CH.isValidSys(msg)) {
-CH.say(msg, replier);
+send {
+try{
+let url = new java.net.URL(AuthURL);
+let con = url.openConnection();
+con.setRequestMethod("POST"); //서버 접속 방법 설정 GET, POST, OPTIONS
+con.setRequestProperty("Content-Type", "application/json; charset=utf-8"); //서버 연결시 가져올 데이터의 형식
+con.setRequestProperty("Authorization", "Basic " + AuthID); //인증키 입력
+con.setRequestProperty("User-Agent", "Mozilla"); //일부의 경우, User-Agent를 요구
+con.setRequestProperty("Accpet", "*.*"); //일부의 경우, 이 헤더가 없으면 오류가 발생
+con.setDoOutput(true);
+let wr = new java.io.DataOutputStream(con.getOutputStream());
+let writer = new java.io.BufferedWriter(new java.io.OutputStreamWriter(wr, "UTF-8"));
+writer.write(JSON.stringify(jsondata));
+writer.close();
+wr.close();
+let responseCode = con.getResponseCode();
+let br;
+if (responseCode == 200) {
+br = new java.io.BufferedReader(new java.io.InputStreamReader(con.getInputStream()));
+} else {
+br = new java.io.BufferedReader(new java.io.InputStreamReader(con.getErrorStream()));
+let inputLine;
+let response = "";
+while ((inputLine = br.readLine()) != null) {
+response += inputLine;
 }
-  
-if (room == "지구b" && CH.isValidMsg(msg)) {
-CH.rsay(4, msg, replier);
+br.close();
+return response;
+} catch (e) {
+return e;
 }
-  
-if (room == "삐약b" && CH.isValidMsg(msg)) {
-CH.rsay(5, msg, replier);
 }
-  
-if (room == "요정b" && CH.isValidMsg(msg)) {
-CH.rsay(6, msg, replier);
+let results = JSON.parse(send());
+replier.reply(results["response"]["replies"][0]["text"]);
+}
 }
 
 }
