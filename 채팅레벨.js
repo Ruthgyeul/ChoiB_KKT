69830e6ef29032fc1df9f4ea1939981a112e6ea8
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2021-01-13 18:46:49 -0600

    Update 채팅레벨.js

--- 채팅레벨.js
+++ 채팅레벨.js
@@ -8,15 +8,9 @@ if (msg.indexOf(Extra[n]) != -1) return true;
 return false;
 };
 
-CH.EsterEgg = function(msg) {
-var Egg = ["체온아 사랑해", "체온아 잘했어", "체온아 멋있다", "체온아 힘내"];
-for (var n = 0; n < Egg.length; n++) {
-if (msg == (Egg[n])) return true; 
-}
-return false;
-};
-
 function response(room, msg, sender, igc, replier){
+
+if (room.indexOf("b")) {
 
 const FS = FileStream, path = "/sdcard/ProjectK/" + room + "/lv.json";
 if(!new java.io.File(path).canRead()) FS.write(path, '[]');
@@ -28,11 +22,11 @@ lv.push({'name':sender, 'lv':1, 'xp':0});
 
 lv[lv.findIndex(e=>e.name==sender)].xp++;
 
-if (lv[lv.findIndex(e=>e.name==sender)].xp>=Number((lv[lv.findIndex(e=>e.name==sender)].lv*25) + 35)) {
+if (lv[lv.findIndex(e=>e.name==sender)].xp >= ((lv[lv.findIndex(e=>e.name==sender)].lv*25) + 35))) {
 lv[lv.findIndex(e=>e.name==sender)].lv++;
-lv[lv.findIndex(e=>e.name==sender)].xp -= ((lv[lv.findIndex(e=>e.name==sender)].lv*25) + 35);
+lv[lv.findIndex(e=>e.name==sender)].xp -= (((lv[lv.findIndex(e=>e.name==sender)].lv-1)*25) + 35);
 FS.write(path, JSON.stringify(lv));
-replier.reply("『 GG " + sender + "레벨업! 』\n" + Number(lv[lv.findIndex(e=>e.name==sender)].lv - 1) + " >> " + lv[lv.findIndex(e=>e.name==sender)].lv + "LV [" + lv[lv.findIndex(e=>e.name==sender)].xp + "/" + ((lv[lv.findIndex(e=>e.name==sender)].lv*25) + 35) + " exp left]");
+replier.reply("『GG " + sender + "레벨업!』\n" + (lv[lv.findIndex(e=>e.name==sender)].lv - 1) + " >> " + lv[lv.findIndex(e=>e.name==sender)].lv + "LV [" + lv[lv.findIndex(e=>e.name==sender)].xp + "/" + ((lv[lv.findIndex(e=>e.name==sender)].lv*25) + 35) + " exp left]");
 }
 
 if (msg == "이모티콘을 보냈습니다.") {
@@ -45,20 +39,17 @@ lv[lv.findIndex(e=>e.name==sender)].xp+5;
 FS.write(path, JSON.stringify(lv));
 }
 
-if (CH.EsterEgg(msg)) {
-lv[lv.findIndex(e=>e.name==sender)].xp+14;
-FS.write(path, JSON.stringify(lv));
-}
-
 if (msg == "!Lrank") {
-replier.reply("『 ⭐ Chat Level List 』" + "\u200b".repeat(500) + "\n" + "═".repeat(20) + "\n\n" + lv.sort((a,b)=>(((b.lv*25)+35)+b.xp)-(((a.lv*25)+35)+a.xp)).slice(0,150).map((e,i)=>++i + "위 [" + e.lv + "LV, " + e.xp + "/" + ((e.lv*25) + 35) + " exp left] : " + e.name).join("\n\n") + "\n\n" + "═".repeat(20));
+replier.reply("『 ⭐ Chat Level List 』" + "\u200b".repeat(500) + "\n" + "═".repeat(20) + "\n\n" + lv.sort((a,b)=>((b.lv*100)+b.xp)-((a.lv*100)+a.xp)).slice(0,150).map((e,i)=>++i + "위 [ " + e.lv + "LV, " + e.xp + "/" + ((e.lv*25) + 35) + " exp left ] : " + e.name).join("\n\n") + "\n\n" + "═".repeat(20));
 }
 
 if (msg == "!level") {
-let index = lv.sort((a,b)=>(((b.lv*25)+35)+b.xp)-(((a.lv*25)+35)+a.xp)).findIndex(e=>e.name==sender);
+let index = chat.sort((a,b)=>((b.lv*100)+b.xp)-((a.lv*100)+a.xp).findIndex(e=>e.name==sender);
 replier.reply("『 ⭐ " + sender + " 』\n" + "═".repeat(15) + "\n『🔥』 순위 : " + (index+1) + "위\n『🔥』 레벨 : " + lv[index].lv + "LV\n『🔥』 남은 경험치 : " + lv[index].xp + "/" + ((lv[index].lv*25) + 35) + " exp\n" + "═".repeat(15));
 }
 
 FS.write(path, JSON.stringify(lv));
 
+}
+  
 }
