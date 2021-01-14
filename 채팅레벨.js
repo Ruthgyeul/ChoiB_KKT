b8674c7f7b05a9bc0702aed61f2237fc03f696ff
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2021-01-13 20:25:30 -0600

    Update 채팅레벨.js

--- 채팅레벨.js
+++ 채팅레벨.js
@@ -26,16 +26,22 @@ if (lv[lv.findIndex(e=>e.name==sender)].xp >= ((lv[lv.findIndex(e=>e.name==sende
 lv[lv.findIndex(e=>e.name==sender)].lv++;
 lv[lv.findIndex(e=>e.name==sender)].xp -= (((lv[lv.findIndex(e=>e.name==sender)].lv-1)*25) + 35);
 FS.write(path, JSON.stringify(lv));
-replier.reply("『GG " + sender + "레벨업!』\n" + (lv[lv.findIndex(e=>e.name==sender)].lv - 1) + " >> " + lv[lv.findIndex(e=>e.name==sender)].lv + "LV [" + lv[lv.findIndex(e=>e.name==sender)].xp + "/" + ((lv[lv.findIndex(e=>e.name==sender)].lv*25) + 35) + " exp left]");
+replier.reply("『GG " + sender + " 레벨업!』\n" + (lv[lv.findIndex(e=>e.name==sender)].lv - 1) + " >> " + lv[lv.findIndex(e=>e.name==sender)].lv + "LV [" + lv[lv.findIndex(e=>e.name==sender)].xp + "/" + ((lv[lv.findIndex(e=>e.name==sender)].lv*25) + 35) + " exp left]");
 }
 
 if (msg == "이모티콘을 보냈습니다.") {
-lv[lv.findIndex(e=>e.name==sender)].xp+2;
+lv[lv.findIndex(e=>e.name==sender)].xp++;
+lv[lv.findIndex(e=>e.name==sender)].xp++;
+lv[lv.findIndex(e=>e.name==sender)].xp++;
 FS.write(path, JSON.stringify(lv));
 }
 
 if (CH.ExtraExp(msg)) {
-lv[lv.findIndex(e=>e.name==sender)].xp+5;
+lv[lv.findIndex(e=>e.name==sender)].xp++;
+lv[lv.findIndex(e=>e.name==sender)].xp++;
+lv[lv.findIndex(e=>e.name==sender)].xp++;
+lv[lv.findIndex(e=>e.name==sender)].xp++;
+lv[lv.findIndex(e=>e.name==sender)].xp++;
 FS.write(path, JSON.stringify(lv));
 }
 
