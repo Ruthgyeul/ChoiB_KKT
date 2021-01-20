d62792e9de59fc7964815875dd35983880b6266e
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2021-01-19 22:28:25 -0600

    v1.0

--- 선택.js
+++ 선택.js
@@ -2,8 +2,8 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB){
 
 if (msg.startsWith("!골라")) {
 var a = msg.trim().substring(4);
-var b = a.split(", ");
-replier.reply("음... 골라볼까요?");
+var b = a.split(" ");
+replier.reply("음... 골라볼까?");
 java.lang.Thread.sleep(3000);
 var N = Math.floor(Math.random()*b.length);
 replier.reply("난 이게 좋은거 같아!\n『 "+ b[N] + " 』");
