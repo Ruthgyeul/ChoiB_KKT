535efe699fcf7932ac736cb7b5cfaec4fe1ca71c
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2021-01-23 20:27:08 -0600

    Update 선택.js

--- 선택.js
+++ 선택.js
@@ -4,7 +4,7 @@ if (msg.startsWith("!골라")) {
 var a = msg.trim().substring(4);
 var b = a.split(" ");
 replier.reply("음... 골라볼까?");
-java.lang.Thread.sleep(3000);
+java.lang.Thread.sleep(2000);
 var N = Math.floor(Math.random()*b.length);
 replier.reply("난 이게 좋은거 같아!\n『 "+ b[N] + " 』");
 }
