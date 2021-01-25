d04546b348562b16317df666ea475ab67907b6cc
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2021-01-25 08:15:33 -0600

    ??

--- 가챠.js
+++ 가챠.js
@@ -1,94 +1,96 @@
+const star3 = [];
+const star4 = [];
+const Pstar4 = [];
+const star5 = [];
+const Pstar5
 var girin = 0;
 
-function response(room, msg, sender, isGroupChat, replier, ImageDB)
-{
- var namu = msg.split(" ");
- 
- if(msg == "/프리코네"){ 
-   girin = 0;
-   var stat = read("priconne",sender);
-   if (stat != undefined){
-     stat = stat.split(",");
-   } else {
-       save("priconne",sender,"0,0");
-    stat = read("priconne",sender);
-    stat = stat.split(",");
-   }
-   stat[0] = stat[0] * 1;
-   stat[1] = stat[1] * 1;
-      var result = "";
-      for (var i = 1; i < 11; i++){
-     if(i != 10){
-    if (i % 2 == 0){
-      result = result + priconne(i)+"\n";
-   } else {
-   result = result + priconne(i)+"\t";  
-   }
-    } else {
-   result = result + priconne(i);
-    }
-   }
-   girin = girin * 1;
-   
-   stat[0] = stat[0]+10;
-   stat[1] = stat[1]+girin;
-   
-   var per = ((stat[1] / stat[0]) * 100).toFixed(2);
-   var money = (stat[0] / 10) * 27000;
-   replier.reply("["+sender+"님의 확률]\n"+"ㆍ뽑은 횟수 : "+stat[0]+"번\nㆍ총 획득 ★3 : "+stat[1]+"개 ("+per+"%)\n\n"+result+"\n\nㆍ가챠 환산 금액 : "+addComma(money)+"원");
-   save("priconne",sender,stat[0]+","+stat[1]);
- 
-    }
-   
-   if(namu[0] == "/가챠삭제"){
-    if(sender == "name"){
-      save("priconne",namu[1],"0,0");
-      replier.reply("가챠기록 삭제 완료");
-    } 
-    else {
-   repiler.reply("권한 부족!")
-    }
-   }
+GSI = function(i){
+var num = Math.random();
+var chara = "";
+if (num <= 0.025) {
+chara = "5성";
+} else if (num <= 0.180){
+chara = "4성";
+} else {
+chara = "3성";
 }
 
-function priconne(i){
- var num = Math.random();
- var chara = "";
- if (num <= 0.025){
-  chara = "3성";
- } else if (num <= 0.180){
-  chara = "2성";
- } else {
-  chara = "1성";
- }
+if (i == 10){
+chara = "4성";
+}
  
- //마지막 뽑기는 1성안나옴
- if (i == 10){
-  if(num <= 0.025){
-   chara = "3성";
-  } else {
-   chara = "2성";
-  }
- }
- 
- if (chara == "1성"){
-  chara = randomItem(star1);
- } else if (chara == "2성"){
-  chara = randomItem(star2);
- } else if (chara == "3성"){
-  chara = randomItem(star3);
-  girin = girin + 1;
- }
- 
- return chara;
+if (i == 90){
+chara = "5성";
 }
 
-function randomItem(a) {
-  return a[Math.floor(Math.random() * a.length)];
+if (chara == "3성"){
+chara = randomItem(star3);
+} else if (chara == "4성"){
+chara = randomItem(star4);
+} else if (chara == "5성"){
+chara = randomItem(star5);
+girin = girin + 1;
 }
 
-function addComma(num) {
-  var regexp = /\B(?=(\d{3})+(?!\d))/g;
-  return num.toString().replace(regexp, ',');
+return chara;
+};
+
+randomItem = function(a) {
+return a[Math.floor(Math.random() * a.length)];
+};
+
+addComma = function(num) {
+var GSL = Number(stat[0]*160);
+};
+
+function response(room, msg, sender, isGroupChat, replier, ImageDB) {
+var namu = msg.split(" ");
+ 
+if (msg == "!가챠") {
+girin = 0;
+var stat = read("GSI",sender);
+
+if (stat != undefined) {
+stat = stat.split(",");
+} else {
+save("GSI",sender,"0,0");
+stat = read("GSI",sender);
+stat = stat.split(",");
 }
-[출처] 프리코네 가챠 시뮬레이터 (카카오톡 봇 커뮤니티) | 작성자 Findragon
+stat[0] = stat[0] * 1;
+stat[1] = stat[1] * 1;
+var result = "";
+
+for (var i = 1; i < 91; i++) {
+if (i != 90) {
+if (i % 2 == 0) {
+result = result + GSI(i)+"\n";
+} else {
+result = result + GSI(i)+"\t";  
+}
+} else {
+result = result + GSI(i);
+}
+}
+girin = girin * 1;
+   
+stat[0] = stat[0]+10;
+stat[1] = stat[1]+girin;
+
+var per = ((stat[1] / stat[0]) * 100).toFixed(2);
+var money = (stat[0] / 10) * 27000;
+replier.reply("[ "+ sender + "님의 확률 ]\n" + "ㆍ뽑은 횟수 : " + stat[0] + "번\nㆍ총 획득 ★3 : " + stat[1] + "개 (" + per + "%)\n\n" + result + "\n\nㆍ가챠 필요 원석 수 : " + addComma(money) + "개");
+save("GSI",sender,stat[0]+","+stat[1]);
+}
+
+if (namu[0] == "!가챠삭제") {
+if (sender == "name") {
+save("GSI",namu[1],"0,0");
+replier.reply("가챠 기록 삭제 완료");
+} else {
+repiler.reply("권한 부족!")
+}
+}
+
+}
