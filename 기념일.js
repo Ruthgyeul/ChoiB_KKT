166b459236b4a37124dbc034fb9d5e9794951c04
R. 체른 <72477746+Rutheon@users.noreply.github.com>
2021-01-27 14:30:36 -0600

    Update 기념일.js

--- 기념일.js
+++ 기념일.js
@@ -9,34 +9,86 @@ var timestr = mhourstr + "시 " + date.getMinutes() + "분 " + date.getSeconds()
 return datestr + " " + timestr;
 }
 
-function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+const baseModule = require("baseval.js");
+const dateModule = require("date.js");
 
-if (msg == "!세계시간") {
-replier.reply("[세계시간]" + Utils.compress() + "\n\n\n[대한민국]\n" + Time(1, -9) + "\n\n[미서부]\n" + Time(0, -1) + "\n\n[미중부]\n" + Time(0, 0) + "\n\n[미동부]\n" + Time(0, 1) + "\n\n[프랑스]\n" + Time(0, 7));
-}
- 
-if (msg == "!한국시간") {
-replier.reply("[한국시간]\n" + Time(1, -9));
-}
- 
-if (msg == "!미국시간") {
-replier.reply("[미서부]\n" + Time(0, -1) + "\n[미중부]\n" + Time(0, 0) + "\n[미동부]\n" + Time(0, 1));
-}
- 
-if (msg == "!미서부시간") {
-replier.reply("[미서부시간]\n" + Time(0, -1));
-}
- 
-if (msg == "!미중부시간") {
-replier.reply("[미중부시간]\n" + Time(0, 0));
-}
- 
-if (msg == "!미동부시간") {
-replier.reply("[미동부시간]\n" + Time(0, 1));
-}
- 
-if (msg == "!프랑스시간") {
-replier.reply("[프랑스시간]\n" + Time(0, 7));
-}
- 
+const bv = new baseModule();
+const date = new dateModule();
+
+const THIS_FILENAME = "Birthday";
+const ROOM_LIST = ["서지원", "유학생들모여라", "뽀로로와친구들"];
+const birthdaypath = "birthdaypath.txt";
+const iscelpath = "iscelebrated.txt";
+
+// 오전에 한번, 오후에 한번 생일축하
+const prevampm = {};
+
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+    try {
+        if (ROOM_LIST.indexOf(room) == -1) {
+            return;
+        }
+
+        var list = msg.split(" ");
+        var cmd = list[0];
+        var option = [];
+        var nbcmd = msg.replace(/\s/g, "");
+        var nbsender = sender.replace(/\s/g, "");
+
+        // options after cmd divided by space
+        for (let i = 0; i < list.length; i++) {
+            if (i != 0) {
+                option[i-1] = list[i];
+            }
+        }
+
+        // 오전 오후인지 구별
+        var isam = (date.getDate("hour")/12 < 1) ? true : false;
+
+        // false 0, true 1
+        var isdone = bv.ReadFile(room, iscelpath);
+        // 최초 실행시 초기화
+        if (isdone == "")
+            isdone = "0";
+
+        if (prevampm[room] == null)
+            prevampm[room] = isam;
+        
+        // 오전 오후가 바뀔때마다 초기화
+        if (prevampm[room] != isam) {
+            isdone = "0";
+        }
+
+        if (isdone == "0") {
+            let datalist = ReadList(room);
+            let blist = [];
+
+            datalist.forEach(function(value) {
+                if (value.reply == date.getDate("month_day")) {
+                    blist.push(value.cmd);
+                }
+            });
+
+            if (blist != null) {
+                blist.forEach(function(value) {
+                    let name = value.replace("!", "").replace("생일", "");
+                    let bmsg = "🥳🎉";
+
+                    for (let i = 0; i < 50; i++) {
+                        bmsg += (name + "님의 생일을 축하합니다!! 🥳🎂🎁🎈🎉🕯🍰");
+                    }
+
+                    replier.reply("🥳🎉오늘 " + date.getDate("month_day") + "은 " + name + "님의 생일입니다! 다들 격하게 생일을 축하해주시길 바랍니다~~🥳🎉\n\n혹시라도 " + name + "님이 이 메시지를 못 볼 수 있으니 @ 언급으로 알려주시길 바랍니당!\n\n만약 " + name + "님이 방을 이미 나가신 경우에는 개인톡으로 축하해주시면 감사하겠습니다😀");
+                    replier.reply(bmsg);
+                });
+            }
+            
+            isdone = "1";
+        }
+
+        prevampm[room] = isam;
+        bv.WriteFile(isdone, room, iscelpath);
+    } catch(e) {
+        Log.debug(e + ", line: " + e.lineNumber + " from " + room);
+    }
 }
