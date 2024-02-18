20fa0a2bffb0b71be2702a41dd21536f0e645a22
RuthGyeul <wingd4705@gmail.com>
2024-02-18 21:44:46 +0900

    Update ㅅㅂㅍ.js

--- ㅅㅂㅍ.js
+++ ㅅㅂㅍ.js
@@ -1,24 +1,20 @@
 var chatCount = {};
 
 function response(room, msg, sender, isGroupChat, _replier, ImageDB) {
-
-if (room.indexOf("b")) {
-
-var replier = {};
-
-replier.reply = function(msg) {
-_replier.reply(msg);
-chatCount[room] = 0;
-};
-
-if (chatCount[room] == undefined) chatCount[room] = 0;
-chatCount[room]++;
-
-if (chatCount[room] >= 300) {
-replier.reply("우왕! ㅅㅂㅍ이다!");
-chatCount[room] = 0;
-}
-
-}
-
+    if (room.indexOf("b")) {
+        var replier = {};
+        
+        replier.reply = function(msg) {
+            _replier.reply(msg);
+            chatCount[room] = 0;
+        };
+        
+        if (chatCount[room] == undefined) chatCount[room] = 0;
+        chatCount[room]++;
+        
+        if (chatCount[room] >= 300) {
+            replier.reply("우왕! ㅅㅂㅍ이다!");
+            chatCount[room] = 0;
+        }
+    }
 }
